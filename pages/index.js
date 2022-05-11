91e901ff13d7ab4bfab9c85962ed508c0e45392f
ibrahim shittu <ibshittu01@gmail.com>
2022-05-11 14:53:49 +0100

    meetup index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import MeetupList from '../components/meetups/MeetupList'
+
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'Meetup 1',
+    image: 'https://cdn.pixabay.com/photo/2020/06/01/22/23/eye-5248678__340.jpg',
+    address: 'Meetup Address 1'
+  },
+  {
+    id: 'm2',
+    title: 'Meetup 2',
+    image: 'https://cdn.pixabay.com/photo/2020/06/01/22/23/eye-5248678__340.jpg',
+    address: 'Meetup Address 2'
+  },
+  {
+    id: 'm3',
+    title: 'Meetup 3',
+    image: 'https://cdn.pixabay.com/photo/2020/06/01/22/23/eye-5248678__340.jpg',
+    address: 'Meetup Address 3'
+  },
+]
+
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <MeetupList meetups={DUMMY_MEETUPS}/>
   )
 }
 
